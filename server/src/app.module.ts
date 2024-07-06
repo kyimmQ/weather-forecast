4b2a67881bb637d01aa659e1744d256f462b155d
Các Lê <cacphuong.le@gmail.com>
2024-07-06 16:42:34 +0700

    Modify TypeORM for deploy environment

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailService } from './email/email.service';
 import { SubscriptionModule } from './subscription/subscription.module';
@@ -12,15 +12,31 @@ import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: 'subscriptions',
-      host: 'localhost',
-      port: 5432,
-      password: 'caclepg',
-      username: 'postgres',
-      entities: [Subscription],
-      synchronize: true,
+    // for local
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //     ssl: {
+    //       rejectUnauthorized: false,
+    //     },
+    //     database: new ConfigService().get<string>('DB_DATABASE'),
+    //     host: new ConfigService().get<string>('DB_HOST'),
+    //     port: Number(new ConfigService().get<string>('DB_PORT')),
+    //     password: new ConfigService().get<string>('DB_USERNAME'),
+    //     username: new ConfigService().get<string>('DB_PASSWORD'),
+    //     entities: [Subscription],
+    //     synchronize: true,
+    // })
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: [Subscription],
+        url: `postgresql://${configService.get<string>('DB_USERNAME')}:${configService.get<string>('DB_PASSWORD')}@${configService.get<string>('DB_HOST')}`,
+      }),
+      inject: [ConfigService],
     }),
 
     ConfigModule.forRoot({ isGlobal: true }),
